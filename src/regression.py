fe4073e94dbc7241cf321abf7e5ad13d1b606534
Toni Kansanoja <toni.kansanoja@gmail.com>
2023-12-05 16:30:33 +0200

    No changes.

--- src/regression.py
+++ src/regression.py
@@ -1,5 +1,8 @@
-from preprocess import *
 import numpy as np
+import matplotlib.pyplot as plt
+from preprocess import *
+
+# IMPORTS FOR REGRESSION RELATED OPERATIONS
 from sklearn.model_selection import cross_validate
 from sklearn.dummy import DummyRegressor
 from sklearn.ensemble import RandomForestRegressor
@@ -7,11 +10,13 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.metrics import r2_score as R2
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
+from sklearn.feature_selection import SelectFromModel
 
 def do_cv(model, X_train, y_train):
     """ Do 5-fold CV on the given model. Returns the estimator which
     performed the best. """
+
+    print("Performing cross validation...")
 
     score = cross_validate(model, 
                             X_train, 
@@ -23,7 +28,6 @@ def do_cv(model, X_train, y_train):
     best_index = np.argmax(score['test_score'])
     print("Best score = ", score['test_score'][best_index])
     return score['estimator'][best_index]
-
 
 def normalize_dataset(dataset):
     scaler = StandardScaler()
@@ -48,16 +52,14 @@ edf_test = apply_encoding(df_test.drop('Id', axis=1), enc)  # encode test data w
 
 X_train = normalize_dataset(edf_train.loc[:,edf_train.columns != 'pSat_Pa'])
 y_train = np.log10(edf_train.loc[:, 'pSat_Pa'])
-x_test = normalize_dataset(edf_test)
+X_test = normalize_dataset(edf_test)
 
-#lr_model = LinearRegression()
-#dr_model = DummyRegressor(strategy='median')
-rf_model = RandomForestRegressor(n_jobs=-1, n_estimators=1000, random_state=2)
+RF = RandomForestRegressor(n_jobs=-1, n_estimators=100, random_state=2)
+RF = RF.fit(X_train, y_train)
 
+model = do_cv(RF, X_train, y_train)
+model = model.fit(X_train, y_train)
 
-best_model = do_cv(rf_model, X_train, y_train)
-
-predictor = best_model.fit(X_train, y_train)
-y_pred = predictor.predict(x_test)
+y_pred = model.predict(X_test)
 
 write_to_file(df_test, y_pred, "predictions.csv")
